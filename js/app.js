9c4930b30ac4d43049737a81f70f06744f47d177
Christian Irack <christian@irack.mx>
2017-02-23 00:58:10 -0600

    Estrellas moviendose y escalandose

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var startsNum = 1000;
+var anims = ['anim1','anim2','anim3','anim4','anim5'];
+document.addEventListener('DOMContentLoaded', function(){
+	for(var x = 0; x < startsNum; x ++){
+		var xRandomFrom = Math.floor((Math.random() * 100) + 1);
+		var yRandomFrom = Math.floor((Math.random() * 100) + 1);
+		var main = document.getElementById('stars');
+		var star = document.createElement('div');
+		star.style.left=xRandomFrom+'%';
+		star.style.top=yRandomFrom+'%';
+		var rand = anims[Math.floor(Math.random() * anims.length)];
+		star.className=rand;
+		var element = main.appendChild(star);
+	}
+});
